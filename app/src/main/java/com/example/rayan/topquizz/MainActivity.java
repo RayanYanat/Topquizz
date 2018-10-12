1102239df7026a27c15be9a3683f3361750358bd
Rayan <rayan.yanat@gmail.com>
2018-10-12 20:07:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rayan/topquizz/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.rayan.topquizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+         * referencement des éléments graphiques dans notre activité
+         */
+
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
+
+        /**
+         * desactive le bouton tant que l'utilisateur n'a pas saisi son prenom
+         */
+        mPlayButton.setEnabled(false);
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPlayButton.setEnabled(s.toString().length()!= 0);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        /**
+         * permet d'etre notifié quand l'utilisateur appuie sur le bouton
+         */
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
