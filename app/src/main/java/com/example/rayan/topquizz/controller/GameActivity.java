99b50895c3ee8b9edec9b1632ebc1398bc80c119
Rayan <rayan.yanat@gmail.com>
2018-10-14 17:58:06 +0200

    déclaration d'un attribut du type QuestionBank + ajout d'une méthode permettant de renvoyer un objet du type QuestionBank.

--- app/src/main/java/com/example/rayan/topquizz/controller/GameActivity.java
+++ app/src/main/java/com/example/rayan/topquizz/controller/GameActivity.java
@@ -6,6 +6,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.rayan.topquizz.R;
+import com.example.rayan.topquizz.model.Question;
+import com.example.rayan.topquizz.model.QuestionBank;
+
+import java.util.Arrays;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -15,16 +19,29 @@ public class GameActivity extends AppCompatActivity {
    private Button mAnswerBtn3;
    private Button mAnswerBtn4;
 
+   private QuestionBank mQuestionBank;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mQuestionBank= this.generateQuestions();
+
 
         mQuestionText = (TextView) findViewById(R.id.activity_game_question_text);
         mAnswerBtn1 = (Button) findViewById(R.id.activity_game_answer1_btn);
         mAnswerBtn2 = (Button) findViewById(R.id.activity_game_answer2_btn);
         mAnswerBtn3 = (Button) findViewById(R.id.activity_game_answer3_btn);
         mAnswerBtn4 = (Button) findViewById(R.id.activity_game_answer4_btn);
+    }
+
+    private QuestionBank generateQuestions(){
+        Question question1 = new Question("What is the name of the current french president?",
+                Arrays.asList("François Hollande", "Emmanuel Macron", "Jacques Chirac", "François Mitterand"),
+                1);
+
+        return new QuestionBank(Arrays.asList(question1));
     }
 
 }
